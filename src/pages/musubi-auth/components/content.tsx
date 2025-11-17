9f557215993cc26fe6e140d19ece6d68fc7d722a
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2025-11-17 12:57:39 +0000

    refactor: Simplify Musubi Auth page by removing unused widgets and updating layout to include only the browserTime widget

--- src/pages/musubi-auth/components/content.tsx
+++ src/pages/musubi-auth/components/content.tsx
@@ -2,22 +2,12 @@ import React from 'react';
 
 import Grid from '@cloudscape-design/components/grid';
 
-import { BaseStaticWidget, bookmarks, browserTime, schedule, spending, todos } from '../widgets';
+import { BaseStaticWidget, browserTime } from '../widgets';
 
 export function Content() {
   return (
-    <Grid
-      gridDefinition={[
-        { colspan: { l: 4, m: 6, default: 12 } },
-        { colspan: { l: 4, m: 6, default: 12 } },
-        { colspan: { l: 4, m: 6, default: 12 } },
-        { colspan: { l: 4, m: 6, default: 12 } },
-        { colspan: { l: 4, m: 6, default: 12 } },
-      ]}
-    >
-      {[todos, schedule, bookmarks, browserTime, spending].map((widget, index) => (
-        <BaseStaticWidget key={index} config={widget.data} />
-      ))}
+    <Grid gridDefinition={[{ colspan: { l: 4, m: 6, default: 12 } }]}>
+      <BaseStaticWidget config={browserTime.data} />
     </Grid>
   );
 }
--- src/pages/musubi-auth/widgets/index.ts
+++ src/pages/musubi-auth/widgets/index.ts
@@ -1,6 +1,2 @@
 export { BaseStaticWidget } from './base-static-widget';
-export { bookmarks } from './bookmarks';
 export { browserTime } from './browser-time';
-export { schedule } from './schedule';
-export { spending } from './spending';
-export { todos } from './todos';
